3ecdec986a03f2aa2d863d088dc4a531a6f48d66
Katarzyna Pacewicz <katarzyna@pacewi.cz>
2021-05-12 15:13:45 +0200

    init commit

---
+++ src/main/java/org/vaadin/example/Application.java
@@ -0,0 +1,18 @@
+package org.vaadin.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+//    mvn clean package crl+Enter
+//java -jar target/spring-skeleton-1.0-SNAPSHOT.jar
+}
